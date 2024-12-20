d6151bb4a9f442017c2cbe8ceae937b3633f09bc
shivam-kaushik <shivamkaushik2k@gmail.com>
2024-12-20 15:31:38 -0500

    Create 1657. Determine if Two Strings Are Close.py

---
+++ LeetCode/1657. Determine if Two Strings Are Close.py
@@ -0,0 +1,11 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        from collections import Counter
+
+        if set(word1) != set(word2):
+            return False
+            
+        freq1 = Counter(word1)
+        freq2 = Counter(word2)
+        
+        return sorted(freq1.values()) == sorted(freq2.values())
