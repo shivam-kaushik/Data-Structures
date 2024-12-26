80876f33b63bc4fb9f744dd1f0617f84134f07db
shivam-kaushik <shivamkaushik2k@gmail.com>
2024-12-26 13:38:48 -0500

    Update 15. 3Sum.py

--- LeetCode/15. 3Sum.py
+++ LeetCode/15. 3Sum.py
@@ -1,13 +1,47 @@
-nums = [-1,0,1,2,-1,-4]
-hashmap ={}
-lst = []
-for i in range(len(nums)):
-    temparr = nums[:i] + nums[i+1:]
-    hashmap ={}
-    target = -nums[i]
-    for j in range(len(temparr)):
-        remaining = target - temparr[j]
-        if remaining in hashmap:
-            lst.append([nums[i], temparr[j], remaining])
-        hashmap[nums[j]] = j
-print(lst)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(len(nums) - 1):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j,k = i+1, len(nums) - 1
+            target = -nums[i]
+            while j < k:
+                if nums[j] + nums[k] < target:
+                    j += 1
+                elif nums[j] + nums[k] > target:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+        return result
+
+
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()  
+#         result = []
+
+#         for i in range(len(nums)):
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 continue
+
+#             target = -nums[i]
+#             hashmap = {}
+#             for j in range(i + 1, len(nums)):
+#                 remaining = target - nums[j]
+#                 if remaining in hashmap:
+#                     triplet = [nums[i], nums[j], remaining]
+#                     triplet.sort()
+#                     if triplet not in result:
+#                         result.append(triplet)
+#                 hashmap[nums[j]] = j
+
+#         return result
