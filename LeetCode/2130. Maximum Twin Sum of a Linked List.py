c8fc1a8038a71c8ec8eef530d8cba0e8b3eb72e2
shivam-kaushik <shivamkaushik2k@gmail.com>
2024-12-31 12:15:00 -0500

    Create 2130. Maximum Twin Sum of a Linked List.py

---
+++ LeetCode/2130. Maximum Twin Sum of a Linked List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        max_sum = 0
+        first, second = head, prev
+        while second:
+            max_sum = max(max_sum, first.val + second.val)
+            first = first.next
+            second = second.next
+
+        return max_sum
