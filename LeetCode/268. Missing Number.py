1f5b0390f14a9805744863348b437e573ec82221
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-04 12:54:21 -0500

    Create 268. Missing Number.py

---
+++ LeetCode/268. Missing Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return int(len(nums)*(len(nums)+1)/2) - sum(nums)
+
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         for i in range(len(nums)+1):
+#             if i not in nums:
+#                 return i
