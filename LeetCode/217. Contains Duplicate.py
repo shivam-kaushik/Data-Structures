fc43a66ff1b22be1c807c2693ac5e8f200478369
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-12 10:58:44 -0500

    Update 217. Contains Duplicate.py

--- LeetCode/217. Contains Duplicate.py
+++ LeetCode/217. Contains Duplicate.py
@@ -1,3 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         visited = set()
+#         for num in nums:
+#             if num in visited:
+#                 return True
+#             visited.add(num)
+#         return False
