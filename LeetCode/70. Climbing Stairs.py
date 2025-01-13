07ffd2623f283c2ed2aacc59a00f3d50c4009e13
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-13 16:24:28 -0500

    Update 70. Climbing Stairs.py

--- LeetCode/70. Climbing Stairs.py
+++ LeetCode/70. Climbing Stairs.py
@@ -11,3 +11,19 @@ class Solution:
             dp[i] = dp[i-1] + dp[i-2]
         
         return dp[n]
+    
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        first, second = 1, 2
+
+        for i in range(3, n + 1):
+            current = first + second
+            first = second
+            second = current
+
+        return second
