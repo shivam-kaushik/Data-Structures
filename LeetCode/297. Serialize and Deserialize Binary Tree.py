b7f3996b9bd2cdc2934267832398e8bc901efd7f
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-18 21:16:48 -0500

    Update 297. Serialize and Deserialize Binary Tree.py

--- LeetCode/297. Serialize and Deserialize Binary Tree.py
+++ LeetCode/297. Serialize and Deserialize Binary Tree.py
@@ -64,3 +64,64 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+
+
+# # Definition for a binary tree node.
+# # class TreeNode(object):
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+
+# class Codec:
+
+#     def serialize(self, root):
+#         """Encodes a tree to a single string.
+        
+#         :type root: TreeNode
+#         :rtype: str
+#         """
+#         q = collections.deque([root])
+#         res = []
+#         while q:
+#             for i in range(len(q)):
+#                 node = q.popleft()
+#                 if node:
+#                     q.append(node.left)
+#                     q.append(node.right)
+#                     res.append(str(node.val))
+#                 else:
+#                     res.append("null")
+#         return ",".join(res)
+
+#     def deserialize(self, data):
+#         """Decodes your encoded data to tree.
+        
+#         :type data: str
+#         :rtype: TreeNode
+#         """
+#         if data == "null": return None
+#         nodes = data.split(",")
+#         root = TreeNode(nodes[0])
+#         q = collections.deque([root])
+#         i = 1
+#         while q:
+#             for j in range(len(q)):
+#                 node = q.popleft()
+#                 if nodes[i]!="null":
+#                     node.left = TreeNode(nodes[i])
+#                     q.append(node.left)
+#                 i+=1
+#                 if nodes[i]!="null":
+#                     node.right = TreeNode(nodes[i])
+#                     q.append(node.right)
+#                 i+=1
+
+#         return root
+
+
+# # Your Codec object will be instantiated and called as such:
+# # ser = Codec()
+# # deser = Codec()
+# # ans = deser.deserialize(ser.serialize(root))
