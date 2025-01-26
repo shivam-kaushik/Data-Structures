1ecf74ff09eb7997b053ff4e125adbadd639241f
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-26 12:53:35 -0500

    Update 138. Copy List with Random Pointer.py

--- LeetCode/138. Copy List with Random Pointer.py
+++ LeetCode/138. Copy List with Random Pointer.py
@@ -7,33 +7,67 @@ class Node:
         self.random = random
 """
 
+
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
         if not head:
             return None
 
+        mapping = {}
         current = head
+
         while current:
-            new_node = Node(current.val)
-            new_node.next = current.next
-            current.next = new_node
-            current = new_node.next
+            mapping[current] = Node(current.val)
+            current = current.next
 
         current = head
         while current:
+            if current.next:
+                mapping[current].next = mapping[current.next]
             if current.random:
-                current.next.random = current.random.next
-            current = current.next.next
+                mapping[current].random = mapping[current.random]
+            current = current.next
 
-        original = head
-        copy = head.next
-        copy_head = copy
+        return mapping[head]
 
-        while original:
-            original.next = original.next.next
-            if copy.next:
-                copy.next = copy.next.next
-            original = original.next
-            copy = copy.next
 
-        return copy_head
+
+# """
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+#         self.val = int(x)
+#         self.next = next
+#         self.random = random
+# """
+
+# class Solution:
+#     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+#         if not head:
+#             return None
+
+#         current = head
+#         while current:
+#             new_node = Node(current.val)
+#             new_node.next = current.next
+#             current.next = new_node
+#             current = new_node.next
+
+#         current = head
+#         while current:
+#             if current.random:
+#                 current.next.random = current.random.next
+#             current = current.next.next
+
+#         original = head
+#         copy = head.next
+#         copy_head = copy
+
+#         while original:
+#             original.next = original.next.next
+#             if copy.next:
+#                 copy.next = copy.next.next
+#             original = original.next
+#             copy = copy.next
+
+#         return copy_head
