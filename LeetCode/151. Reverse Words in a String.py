38c80c1028d9d3b94b145019f2e66bf8c7ddd317
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-27 13:52:15 -0500

    Update 151. Reverse Words in a String.py

--- LeetCode/151. Reverse Words in a String.py
+++ LeetCode/151. Reverse Words in a String.py
@@ -1,3 +1,11 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(list(reversed(s.split())))
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+        words = s.split()
+        words.reverse()
+        return ' '.join(words)
