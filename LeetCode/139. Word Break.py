f7897b7411bf89a803408efd0b73568dc442cc83
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-27 13:46:48 -0500

    Update 139. Word Break.py

--- LeetCode/139. Word Break.py
+++ LeetCode/139. Word Break.py
@@ -17,3 +17,34 @@ class Solution:
             return False
 
         return helper(s)
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+
+        return dp[n]
+    
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         words = set(wordDict)
+#         wordBreakCurr = [False for i in range(len(s))]
+#         for i in range(len(s)):
+#             if s[:i+1] in words:
+#                 wordBreakCurr[i] = True
+#                 continue
+#             for j in range(i): 
+#                 if wordBreakCurr[j] == True and s[j+1:i+1] in words:
+#                     wordBreakCurr[i] = True
+#                     continue
+#         return wordBreakCurr[len(s)-1]
