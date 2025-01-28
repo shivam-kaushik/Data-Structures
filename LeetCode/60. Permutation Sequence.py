757618e0ea8a3f1536a71ffd18681f41bfdc97bc
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-27 20:30:25 -0500

    Update 60. Permutation Sequence.py

--- LeetCode/60. Permutation Sequence.py
+++ LeetCode/60. Permutation Sequence.py
@@ -1,9 +1,28 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
+        import math
+        
+        numbers = list(range(1, n + 1))
+        
+        result = []
+        
+        k -= 1
+        
+        for i in range(n, 0, -1):
+            fact = math.factorial(i - 1)
+            index = k // fact
+            result.append(str(numbers.pop(index)))
+            k %= fact
+        
+        return ''.join(result)
 
-        lst = [i for i in range(1, n+1)]
 
-        all_perms = list(permutations(lst))
-        all_perms.sort()
-        kth_perm = all_perms[k-1]
-        return ''.join(map(str, kth_perm))
+# class Solution:
+#     def getPermutation(self, n: int, k: int) -> str:
+
+#         lst = [i for i in range(1, n+1)]
+
+#         all_perms = list(permutations(lst))
+#         all_perms.sort()
+#         kth_perm = all_perms[k-1]
+#         return ''.join(map(str, kth_perm))
