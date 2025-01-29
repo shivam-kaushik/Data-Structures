abcfef7abbd9f6f504d72864700a28ffe10004fd
shivam-kaushik <shivamkaushik2k@gmail.com>
2025-01-29 14:30:41 -0500

    Update 78. Subsets.py

--- LeetCode/78. Subsets.py
+++ LeetCode/78. Subsets.py
@@ -11,3 +11,24 @@ class Solution:
         result = []
         backtrack(0, [])
         return result
+    
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        sol = []
+
+        def bactrack(i):
+            if i == n:
+                res.append(sol[:])
+                return
+            
+            bactrack(i + 1)
+
+            sol.append(nums[i])
+            bactrack(i + 1)
+            sol.pop()
+        
+        bactrack(0)
+        return res
