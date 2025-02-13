80ff9352af990fe4fb254b8c4f9db67ffe12d03e
shivam-kaushik <56489671+shivam-kaushik@users.noreply.github.com>
2025-02-13 11:35:47 -0500

    Update 152. Maximum Product Subarray.py

--- LeetCode/152. Maximum Product Subarray.py
+++ LeetCode/152. Maximum Product Subarray.py
@@ -10,3 +10,20 @@ class Solution:
             current_min = min(num, num * temp, num * current_min)
             max_product = max(max_product, current_max)
         return max_product
+    
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMin, curMax = 1, 1
+
+        for n in nums:
+            if n==0:
+                curMin, curMax = 1, 1
+                continue
+            temp = curMax * n
+            curMax = max(n*curMax, n*curMin, n)
+            curMin = min(temp, n*curMin, n) 
+            res = max(res, curMax)
+        return res
